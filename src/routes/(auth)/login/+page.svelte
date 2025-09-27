ccc896a45404b04d93845e3b7f4025b457f8ee03
Stefan Leßle <stefan.lessle@mediakular.com>
2025-09-26 20:23:44 -0500

    Reverted test

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -36,8 +36,6 @@
             <label for="password" class="block text-sm/6 font-medium text-gray-900">{m.password()}</label>
             <input type="password" name="password" placeholder={m.password()} id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-green-600 sm:text-sm/6">
         </div>
-
-Test
         
         <div>
             <button type="submit" disabled={loading} class="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-green-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 cursor-pointer">
